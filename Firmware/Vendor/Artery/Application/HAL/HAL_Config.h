610546f3eb03ed19f6e09b7af578cb673ea33aa9
_VIFEXTech <vifextech@foxmail.com>
2025-04-26 00:25:43 +0800

    feat(HAL_Config): re-add v1.1 pin config

--- Firmware/Vendor/Artery/Application/HAL/HAL_Config.h
+++ Firmware/Vendor/Artery/Application/HAL/HAL_Config.h
@@ -25,12 +25,16 @@
 
 /* clang-format off */
 
+#define CONFIG_USE_V1_2_PIN 1
+
 /* Logger Serial */
 #define CONFIG_LOG_SERIAL         Serial
 
 /* Motor */
 #define CONFIG_MOTOR_OUT1_PIN     PA2
 #define CONFIG_MOTOR_OUT2_PIN     PA3
+
+#if CONFIG_USE_V1_2_PIN
 
 /* Buzzer */
 #define CONFIG_BUZZ_PIN           PB1
@@ -41,6 +45,20 @@
 
 /* Button */
 #define CONFIG_BUTTON_SEL_PIN     PA7
+
+#else
+
+ /* Buzzer */
+#define CONFIG_BUZZ_PIN           PB2
+ 
+ /* Battery */
+#define CONFIG_BATT_DET_PIN       PB1
+#define CONFIG_PWR_EN_PIN         PA7
+ 
+ /* Button */
+#define CONFIG_BUTTON_SEL_PIN     PB0
+
+#endif
 
 /* Memory */
 #define CONFIG_MEMORY_STACK_INFO  1 /* Reuqire MPU Disabled */
