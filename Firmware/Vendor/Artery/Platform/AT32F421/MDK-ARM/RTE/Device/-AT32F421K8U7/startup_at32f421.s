6e651c170196f32dafe68b8bb498d422921b1576
_VIFEXTech <vifextech@foxmail.com>
2025-05-11 17:50:09 +0800

    fix(startup_at32f421.s): adjust stack & heap size

--- Firmware/Vendor/Artery/Platform/AT32F421/MDK-ARM/RTE/Device/-AT32F421K8U7/startup_at32f421.s
+++ Firmware/Vendor/Artery/Platform/AT32F421/MDK-ARM/RTE/Device/-AT32F421K8U7/startup_at32f421.s
@@ -11,7 +11,7 @@
 ;   <o> Stack Size (in Bytes) <0x0-0xFFFFFFFF:8>
 ; </h>
 
-Stack_Size      EQU     0x00000640
+Stack_Size      EQU     0x00000700
 
                 AREA    STACK, NOINIT, READWRITE, ALIGN=3
 Stack_Mem       SPACE   Stack_Size
@@ -21,7 +21,7 @@ __initial_sp
 ;   <o>  Heap Size (in Bytes) <0x0-0xFFFFFFFF:8>
 ; </h>
 
-Heap_Size       EQU     0x00002F00
+Heap_Size       EQU     0x00002E00
 
                 AREA    HEAP, NOINIT, READWRITE, ALIGN=3
 __heap_base
